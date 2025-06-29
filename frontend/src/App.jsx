beb30b178aa8b4459aac515697c19b1300320d49
Satya <sa7ya.30@gmail.com>
2025-06-29 15:39:20 +0530

    added routing and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
-import Home from "./pages/Home"
+import './css/App.css';
+import Home from "./pages/Home";
+import Favorites from './pages/Favorites';
+import {Routes,Route} from "react-router-dom"
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <>
-    <Home></Home>
-    </>
-  )
+    <div>
+      <NavBar></NavBar>
+      <main className="main-content">
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/Favorites' element={<Favorites/>}></Route>
+      </Routes>
+    </main>
+    </div>
+  );
 }
 
-export default Home
+export default App
